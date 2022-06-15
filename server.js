1cdad95943bd0810abe9b8c2e08374c6ac081cfa
Rosemarie <rosemarie.m.halim@gmail.com>
2022-06-15 18:37:24 -0400

    Deploying

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
